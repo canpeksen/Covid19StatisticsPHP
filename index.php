7ac98bc5ed69aafd9306755fc1f896f8b89531f1
Dursun Can PEKŞEN <57504463+canpeksen@users.noreply.github.com>
2020-11-27 14:34:38 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Small Business - Start Bootstrap Template</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/small-business.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Heading Row -->
+    <div class="row align-items-center my-5">
+      <div class="col-lg-7">
+        <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
+      </div>
+      <!-- /.col-lg-8 -->
+      <div class="col-lg-5">
+        <h1 class="font-weight-light">Business Name or Tagline</h1>
+        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+        <a class="btn btn-primary" href="#">Call to Action!</a>
+      </div>
+      <!-- /.col-md-4 -->
+    </div>
+    <!-- /.row -->
+
+    <!-- Call to Action Well -->
+    <div class="card text-white bg-info my-5 py-4 text-center">
+      <div class="card-body">
+        <p class="text-white m-0">Veriler <code>http://coronavirus-19-api.herokuapp.com/tabs/tab1</code> sitesinden çekilmektedir. <a href="https://canpeksen.com.tr">Dursun Can PEKŞEN Web Tasarım Hizmetleri</a>  </p>
+      </div>
+    </div>
+    <!-- Content Row -->
+    <div class="row">
+    	<?php 
+$site="https://coronavirus-19-api.herokuapp.com/countries";
+$kr=file_get_contents($site);
+$KrJson=json_decode($kr,true);
+foreach ($KrJson as $key => $value){
+	$krcek=$value["country"]; "<br>";
+	$krcek1=$value["cases"]; "<br>";
+	$krcek2=$value["todayCases"]; "<br>";
+	$krcek3=$value["deaths"]; "<br>";
+	$krcek4=$value["todayDeaths"]; "<br>";
+	$krcek5=$value["recovered"]; "<br>";
+	$krcek6=$value["active"]; "<br>";
+	$krcek7=$value["critical"]; "<br>";
+	$krcek8=$value["casesPerOneMillion"]; "<br>";
+	$krcek9=$value["deathsPerOneMillion"]; "<br>";
+	$krcek10=$value["totalTests"]; "<br>";
+	$krcek11=$value["testsPerOneMillion"]; "<br>";
+	
+	"<br/>"; ?>
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <h2 class="card-title"><?php echo $krcek ?></h2>
+            <p class="card-text"><b>Toplam Vaka:</b> <?php echo $krcek1 ?><br><b>Bugünün Vaka Sayısı: </b><?php echo $krcek2 ?><br><b>Toplam Ölüm Sayısı: </b><?php echo $krcek3 ?><br><b>Bugünün Ölü Sayısı: </b><?php echo $krcek4 ?><br><b>Toplam İyileşen Sayısı: </b><?php echo $krcek5 ?><br><b>Toplam Hasta Sayısı: </b><?php echo $krcek6 ?><br><b>Toplam Kritik Hasta: </b><?php echo $krcek7 ?><br><b>Toplam Test Sayısı: </b><?php echo $krcek10 ?><br></p>
+          </div>
+        </div>
+      </div>
+  <? } ?>
+      <!-- /.col-md-4 -->
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <h2 class="card-title">Card Two</h2>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary btn-sm">More Info</a>
+          </div>
+        </div>
+      </div>
+      <!-- /.col-md-4 -->
+      <div class="col-md-4 mb-5">
+        <div class="card h-100">
+          <div class="card-body">
+            <h2 class="card-title">Card Three</h2>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary btn-sm">More Info</a>
+          </div>
+        </div>
+      </div>
+      <!-- /.col-md-4 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
